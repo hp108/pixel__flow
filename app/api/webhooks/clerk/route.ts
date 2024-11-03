5c94034e179eb3390be8eb5e7502f89cc302b29f
Katakam Hari Prasad <katakamhariprasad@Katakams-MacBook-Air.local>
2024-11-03 09:54:36 +0530

    clerk webhook changes

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -3,7 +3,6 @@ import { headers } from 'next/headers'
 import { WebhookEvent,clerkClient } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
-import User from '@/lib/db/models/user.model'
 
 export async function POST(req: Request) {
 //   You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
