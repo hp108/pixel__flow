e1a95b0800b33a6d81188510541d3a0764e5ccf0
Katakam Hari Prasad <katakamhariprasad@Katakams-MacBook-Air.local>
2024-11-03 09:16:11 +0530

    clerk webhook changes

--- lib/db/models/user.model.ts
+++ lib/db/models/user.model.ts
@@ -3,13 +3,13 @@ import { model, models, Schema } from "mongoose";
 const userSchema = new Schema({
     clerkId:{type:String, required:true , unique:true} ,
     email:{type:String, required:true, unique:true},
-    username:{type:String, unique:true},
+    username:{type:String},
     photo:{type:String, required:true},
     firstName:{type:String},
     lastName:{type:String},
     planId:{type:String, default: 1},
     creditBalance:{type:Number , default: 20},    
-})
+})c
 
 const User =  models?.User  || model('User', userSchema) 
 
